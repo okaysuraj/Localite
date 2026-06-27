e97658dc03cadd466ae4cb9a68f2ca93741f13bf
Suraj Kumar <suraj14879@gmail.com>
2026-06-27 19:09:35 +0530

    Fix PostConstruct import from javax to jakarta for Spring Boot compatibility

--- localite-backend/src/main/java/com/localite/backend/config/FirebaseConfig.java
+++ localite-backend/src/main/java/com/localite/backend/config/FirebaseConfig.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 import org.slf4j.Logger;
